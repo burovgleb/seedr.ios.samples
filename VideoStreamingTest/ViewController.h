8859a2183a9af644f89f9341bec57cd9bd9e6c2b
 Developer <developer@Developers-iMac.local>
2012-08-15 19:43:10 +0400

    Initial Commit

---
+++ VideoStreamingTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  VideoStreamingTest
+//
+//  Created by Developer on 15.08.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
